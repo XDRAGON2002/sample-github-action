6116918e378612eb165a6b727118ef7e53550920
DRAGON <anantvijay3@gmail.com>
2023-01-20 18:47:19 +0530

    Created action

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core")
+const tc = require("@actions/tool-cache")
+
+async function setup() {
+    try {
+        const url = "https://github.com/intel/cve-bin-tool/releases/download/v3.2/cve-bin-tool-3.2.tar.gz"
+        const pathToDownloaded = await tc.downloadTool(url)
+
+        const extract = tc.extractTar
+        const pathToCLI = await extract(pathToDownloaded)
+
+        core.addPath(pathToCLI)
+    } catch (e) {
+        core.setFailed(e)
+    }
+}
+
+module.exports = setup
+
+if (require.main === module) {
+    setup()
+}
